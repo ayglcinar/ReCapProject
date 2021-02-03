6f974db89d89123aef1c4d9872c7e29566264217
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-03 20:14:43 +0300

    Add files via upload

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        public int ID { get; set; }
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public string ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
